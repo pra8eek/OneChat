a4af657898733efa8c1a6337b1ddb7f06bd50e7f
Prateek Bhardwaj <32588984+pra8eek@users.noreply.github.com>
2020-02-06 11:18:55 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -38,10 +38,15 @@ def meme(update, context):
 		print(subr)
 		if subr == "menu" :
 			fetched = True
-			context.bot.send_message(chat_id=update.effective_chat.id, text="Here are the categories: code | meirl | gay")
+			context.bot.send_message(chat_id=update.effective_chat.id, text="Here are the categories: cricket | ouija | code | meirl | gay")
 			context.bot.send_message(chat_id=update.effective_chat.id, text="Happy to add more. You already know the developer")
+			return
 		elif subr == "code" :
 			subreddit = "ProgrammerHumor"
+		elif subr == "ouija" :
+			subreddit = "jesuschristuoija"
+		elif subr == "cricket" :
+			subreddit = "cricketshitpost"	
 		elif subr == "meirl" :
 			subreddit = "me_irl"
 		elif subr == "gay" :
@@ -51,11 +56,17 @@ def meme(update, context):
 	except :
 		subreddit = "2meirl4meirl"
 
+	if not fetched :
+		print("subreddit is: ", subreddit)
+		print("count is: ", k)
+
+	time = "day"
+
 	while not fetched :
 		fetchCount = 0
 
 		print("Searching")
-		for submission in reddit.subreddit(subreddit).top(time_filter = "day", limit = k) :
+		for submission in reddit.subreddit(subreddit).top(time_filter = time, limit = k) :
 			if submission.url[-3:] == "jpg" :
 				fetchCount += 1
 	
@@ -75,15 +86,22 @@ def meme(update, context):
 			print("Sent")
 		else :
 			k = k*2
+			if k > 100 :
+				time = "all"
+			if k > 200 :
+				context.bot.send_message(chat_id=update.effective_chat.id, text="Something went wrong! Try with a lower count")
+
 
 def boob(update, context):
 	try:
 		k = int(context.args[-1])
+		if k > 10 :
+			context.bot.send_message(chat_id=update.effective_chat.id, text="Arre havasi mutthal sale! Jaake pornhub chala")
+			context.bot.send_message(chat_id=update.effective_chat.id, text="10 se zyada nahi milega, apne lund ko control me rakh!")
+			k = 10
 	except :
 		k = 1
-		context.bot.send_message(chat_id=update.effective_chat.id, text="Arre havasi mutthal sale! Jaake pornhub chala")
-		context.bot.send_message(chat_id=update.effective_chat.id, text="10 se zyada nahi milega, apne lund ko control me rakh!")
-		k = 10
+
 	fetched = False
 	kcopy = k
 
@@ -94,45 +112,40 @@ def boob(update, context):
 			context.bot.send_message(chat_id=update.effective_chat.id, text="Here are the categories: " + 
 				"pic | desi | paki | teen | boob | cute | pussy | ass")
 			context.bot.send_message(chat_id=update.effective_chat.id, text="Happy to add more. You already know the developer")
+
 		elif choice == "pic" :
-			choice = "jpg"
 			subreddit = "nsfw"
 		elif choice == "desi" :
-			choice = "jpg"
 			subreddit = "indiangirls"
 		elif choice == "paki" :
-			choice = "jpg"
 			subreddit = "PakiBeauties"
 		elif choice == "teen" :
-			choice = "jpg"
 			subreddit = "legalteens"
 		elif choice == "boob" :
-			choice = "jpg"
 			subreddit = "Boobies"
 		elif choice == "cute" :
-			choice = "jpg"
 			subreddit = "adorableporn"
 		elif choice == "pussy" :
-			choice = "jpg"
 			subreddit = "pussy"
 		elif choice == "ass" :
-			choice = "jpg"
 			subreddit = "ass"
 		else :
-			choice = "jpg"
 			subreddit = "nsfw"
 	except :
-		 choice = "jpg"
 		 subreddit = "nsfw"
 
-	print("choice is: ", choice)
+	if not fetched :
+		print("subreddit is: ", subreddit)
+		print("count is: ", k)
+
+	time = "day"
 
 	while not fetched :
 		fetchCount = 0
 
 		print("Searching")
-		for submission in reddit.subreddit(subreddit).top(time_filter = "day", limit = k) :
-			if submission.url[-3:] == choice :
+		for submission in reddit.subreddit(subreddit).top(time_filter = time, limit = k) :
+			if submission.url[-3:] == "jpg" :
 				fetchCount += 1
 	
 		if fetchCount >=  kcopy:
@@ -140,20 +153,21 @@ def boob(update, context):
 			fetched = True
 			print("Sending ")
 
-			for submission in reddit.subreddit(subreddit).top(time_filter = "day", limit = k) :
-				if submission.url[-3:] == choice :
+			for submission in reddit.subreddit(subreddit).top(time_filter = time, limit = k) :
+				if submission.url[-3:] == "jpg" :
 					sent += 1
-					if choice == "jpg" :
-						context.bot.send_photo(chat_id=update.effective_chat.id, photo=submission.url)
-					elif choice == "gif" :
-						print(submission.url)
-						context.bot.send_animation(chat_id=update.effective_chat.id, animation=submission.url)
-
+					context.bot.send_photo(chat_id=update.effective_chat.id, photo=submission.url)
 					if sent == kcopy :
 						break
 			print("Sent")
 		else :
 			k = k*2
+			k = k*2
+			if k > 100 :
+				time = "all"
+			if k > 200 :
+				context.bot.send_message(chat_id=update.effective_chat.id, text="Something went wrong! Try with a lower count")
+
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="A gaye aap apni ma chudwane!")
